1f8d9ad711f98bb836a9820f146e906ff5b54a5a
fancy9 <fancyIX@yahoo.com>
2015-01-07 23:49:15 -0800

    modified names

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lwthttp -lwt -lboost_signals -lpthread -lmongoclient -lboost_thread -lboost_regex -lboost_system
+
