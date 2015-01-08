a8ac7b551efbedd797006487271a31fb89a90ac7
fancy9 <fancyIX@yahoo.com>
2015-01-07 23:51:19 -0800

    idelete

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lwthttp -lwt -lboost_signals -lpthread -lmongoclient -lboost_thread -lboost_regex -lboost_system
-
