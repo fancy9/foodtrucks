6b15e33cd99d172a22a1112010bd6ad80f65b94e
root <root@back-end.c.wired-standard-817.internal>
2015-01-09 05:47:12 +0000

    importdb

--- importdb.sh
+++ importdb.sh
@@ -1,2 +1,2 @@
 wget https://data.sfgov.org/resource/rqzj-sfat.csv
-mongoimport -d mydb -c things --type csv --file rqzj-sfat.csv --headerline
+mongoimport -d mydb -c things --type csv --file rqzj-sfat.csv --headerline --drop
